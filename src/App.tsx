b0e55dce9bab6e783c9736773716456c35ebf022
indiodev <jhollyfer.fr@gmail.com>
2025-01-31 12:19:01 -0500

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export function App() {
 						className="bg-green-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
 						onClick={() =>
 							generatePDF(targetRef, {
-								method: 'open',
+								// method: 'open',
 								resolution: Resolution.EXTREME,
 								page: {
 									orientation: 'landscape',
